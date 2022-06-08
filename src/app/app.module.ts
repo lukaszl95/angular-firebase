a3c59f7a6b6ce782120ded32661f5ff935aab575
Lukasz <lukaszl95@o2.pl>
2022-06-08 21:24:57 +0200

    Start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAnalyticsModule } from '@angular/fire/compat/analytics';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAnalyticsModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'angular-nauka-6ca80',
+    appId: '1:230057210572:web:08af7ccd14ff37e7c08869',
+    databaseURL: 'https://angular-nauka-6ca80-default-rtdb.firebaseio.com',
+    storageBucket: 'angular-nauka-6ca80.appspot.com',
+    apiKey: 'AIzaSyBx4sI-3LUmZIOgVduWGZoqhgIzEDQ9fag',
+    authDomain: 'angular-nauka-6ca80.firebaseapp.com',
+    messagingSenderId: '230057210572',
+  },
   production: true
 };
